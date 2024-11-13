4789f0c0af3c33139cbee4a65e53a01143dc876e
HeavenSent9 <Consul.KnN@yandex.ru>
2024-11-13 15:30:50 +0300

    new

--- two_two/two_two.py
+++ two_two/two_two.py
@@ -1,3 +1,15 @@
-a = 123
-b = 123
-c = 123
+my_list = [1,2,3,4,"qwe", False,True]
+my_list_2 = list()
+my_list.append(1)
+print(my_list)
+
+my_tuple = (1,2,3)
+print(type(my_tuple))
+my_set = {1,2,3,4,5}
+my_set.add(5)
+my_set.add(6)
+print(my_set)
+print(type(my_set))
+my_dict = {"one":1,"two":2}
+print(type(my_dict))
+print(my_dict.keys())
