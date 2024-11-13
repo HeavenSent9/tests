0a1720b36105c3fc132b1fab1cc0e977fbfb52a3
HeavenSent9 <Consul.KnN@yandex.ru>
2024-11-13 21:42:27 +0300

    fix

--- two_two/two_two.py
+++ two_two/two_two.py
@@ -1,15 +1,25 @@
 my_list = [1,2,3,4,"qwe", False,True]
 my_list_2 = list()
+numb_index = my_list[3]
+num_index = my_list[-1]
 my_list.append(1)
+print(numb_index)
+print(num_index)
 print(my_list)
+list_2 = [1,3,5,5,3]
+list_2 = list(set(list_2))
+print(list_2)
 
 my_tuple = (1,2,3)
 print(type(my_tuple))
 my_set = {1,2,3,4,5}
+my_set_2 = set()
 my_set.add(5)
 my_set.add(6)
 print(my_set)
 print(type(my_set))
 my_dict = {"one":1,"two":2}
+my_dict[3] = 3
 print(type(my_dict))
+print(my_dict)
 print(my_dict.keys())
