dd69a40930a3135b635233d14cb95a453ae274c0
HeavenSent9 <Consul.KnN@yandex.ru>
2024-11-14 22:34:30 +0300

    add

--- two_two/funck.py
+++ two_two/funck.py
@@ -1,7 +1,23 @@
 
 first_num = 10
 second_num = 20
-def calk(num,num_2):
-    print(num+num_2)
+def calk(num):
+    print(num+second_num)
 
-calk(first_num,second_num)
+calk(first_num)
+
+def sum_all(*args):
+    result = 0
+    for i in args:
+        result += i
+    return result
+print(sum_all(1,2,3))
+
+def price_list(**kwargs):
+    print(kwargs)
+    for product in kwargs.items():
+        prod, coast = product
+        print(prod, coast)
+
+price_list(iphone = 100, samsung = 10, nokia = 1)
+    
